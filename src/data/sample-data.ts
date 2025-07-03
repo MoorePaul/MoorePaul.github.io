dc11731d21db3d2c0323cc4a2780501f8a0eac51
MoorePaul <st_rutter@hotmail.com>
2025-07-03 16:11:15 +0900

    Main options finished

--- src/data/sample-data.ts
+++ src/data/sample-data.ts
@@ -86,6 +86,336 @@ import type { DynForm, GroupedOption, SelectData } from "$lib/types";
         price: '132,000円',
         image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
       },
+      {
+        label: 'JOY365 SF 305X BLUE',
+        value: '604885',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      }
+    ]
+  },
+  {
+    name: '特徴',
+    options: [
+      {
+        label: 'JOY365 SF 405 BLUE',
+        value: '604887',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'JOY365 SF 405x BLUE',
+        value: '604888',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'JOY365 SF 305x BLACK/BLUE',
+        value: '604889',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'JOY365 SF 405 BLACK/BLUE',
+        value: '604890',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'JOY365 SF 405x BLACK/BLUE',
+        value: '604891',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'JOY365 SF 305x YELLOW',
+        value: '604892',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'JOY365 SF 405 YELLOW',
+        value: '604893',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'JOY365 SF 405x YELLOW',
+        value: '604894',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      }
+    ]
+  },
+  {
+    name: '特徴',
+    options: [
+      {
+        label: 'Dream7 SF 305x ORANGE',
+        value: '604887',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'Dream7 SF 405 ORANGE',
+        value: '604887',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'Dream7 SF 405x ORANGE',
+        value: '604887',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'Dream7 SF 305x BLACK/GREEN',
+        value: '604887',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'Dream7 SF 405 BLACK/GREEN',
+        value: '604887',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'Dream7 SF 405x BLACK/GREEN',
+        value: '604887',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'Dream7 SF 305x WHITE',
+        value: '604887',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'Dream7 SF 405 WHITE',
+        value: '604887',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'Dream7 SF 405x WHITE',
+        value: '604887',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      }
+    ]
+  },
+  {
+    name: '特徴',
+    options: [
+      {
+        label: 'AutoFelx SF 305x PINK/BLACK',
+        value: '604871',
+        price: '114,400円',
+        image: 'https://static.wixstatic.com/media/1d8757_97315eeb572b49308edd85202b9c7cbf~mv2.png'
+      },
+      {
+        label: 'AutoFelx SF 405 PINK/BLACK',
+        value: '604872',
+        price: '114,400円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'AutoFelx SF 505 PINK/BLACK',
+        value: '604873',
+        price: '114,400円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'AutoFelx SF 305x Yellow',
+        value: '604874',
+        price: '114,400円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'AutoFelx SF 405 Yellow',
+        value: '604875',
+        price: '114,400円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'AutoFelx SF 505 Yellow',
+        value: '604876',
+        price: '114,400円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'AutoFelx SF 305x BLACK',
+        value: '604877',
+        price: '114,400円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'AutoFelx SF 405 BLACK',
+        value: '604878',
+        price: '114,400円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'AutoFelx SF 505 BLACK',
+        value: '604879',
+        price: '114,400円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'AutoFelx SF 305x RAINBOW',
+        value: '604880',
+        price: '114,400円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'AutoFelx SF 405 RAINBOW',
+        value: '604881',
+        price: '114,400円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'AutoFelx SF 505 RAINBOW',
+        value: '604882',
+        price: '114,400円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      }
+    ]
+  },
+  {
+    name: '特徴',
+    options: [
+      {
+        label: 'JOY365 SF 405 PINK',
+        value: '604883',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'JOY365 SF 405x PINK',
+        value: '604884',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'JOY365 SF 305x BLUE',
+        value: '604885',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      }
+    ]
+  },
+  {
+    name: '特徴',
+    options: [
+      {
+        label: 'JOY365 SF 405 BLUE',
+        value: '604887',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'JOY365 SF 405x BLUE',
+        value: '604888',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'JOY365 SF 305x BLACK/BLUE',
+        value: '604889',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'JOY365 SF 405 BLACK/BLUE',
+        value: '604890',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'JOY365 SF 405x BLACK/BLUE',
+        value: '604891',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'JOY365 SF 305x YELLOW',
+        value: '604892',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'JOY365 SF 405 YELLOW',
+        value: '604893',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'JOY365 SF 405x YELLOW',
+        value: '604894',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      }
+    ]
+  },
+  {
+    name: '特徴',
+    options: [
+      {
+        label: 'Dream7 SF 305x ORANGE',
+        value: '604887',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'Dream7 SF 405 ORANGE',
+        value: '604887',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'Dream7 SF 405x ORANGE',
+        value: '604887',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'Dream7 SF 305x BLACK/GREEN',
+        value: '604887',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'Dream7 SF 405 BLACK/GREEN',
+        value: '604887',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'Dream7 SF 405x BLACK/GREEN',
+        value: '604887',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'Dream7 SF 305x WHITE',
+        value: '604887',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'Dream7 SF 405 WHITE',
+        value: '604887',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      },
+      {
+        label: 'Dream7 SF 405x WHITE',
+        value: '604887',
+        price: '132,000円',
+        image: 'https://static.wixstatic.com/media/b166e5_cab70dcd39cb40b591214ffee6afa520~mv2.png'
+      }
     ]
   }
  ];
